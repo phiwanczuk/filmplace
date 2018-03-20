eadcff5509e02af20fe7c4506e44a11256f8f445
Piotrek <phiwanczuk@gmail.com>
2018-03-20 18:43:09 +0100

    fetching added

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 class Movies extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             films: [],
+            isLoading: false,
+            error: null,
         };
     }
 
@@ -12,30 +14,40 @@ class Movies extends React.Component{
 
     ComponentDidMount(){
 
+        this.setState({isLoading: true});
+
         fetch('http://www.omdbapi.com/?i=tt3896198&apikey=59e742a6')
-        .then(results =>{
-            return results.json();
-        }).then(data =>{
-            let films = data.results.map((film) =>{
-                return(
-                    <div key={film.results}>
-                        <h1>{film}</h1>
-                    </div>
-                )
+            .then(response =>{
+                if(response.ok) {
+                    return response.json();
+                }else{
+                    throw new Error('cos sie posypalo...')
+                }
             })
-            this.setState({films: films});
-            console.log("state", this.state.films)
-            console.log(this.state.films)
-        })
+            .then(data => this.setState({films: data.films, isLoading: false}))
+            .catch(error => this.setState({error, isLoading: false}))
     }
 
 
     render(){
+
+        const {films, isLoading,error} = this.state;
+
+        if(error) {
+            return <p>{error.message}</p>
+        }
+
+        if(isLoading) {
+            return <p>Loading ...</p>;
+        }
+
         return(
             <div>
-                {this.state.films}
-
-            </div>
+                {films.map(film =>
+                    <div key={film.objectID}>
+                        <a href={film.url}>{film.title}</a>
+                    </div>
+                )}</div>
         )
     }
 }
