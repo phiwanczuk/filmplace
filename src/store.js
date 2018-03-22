6b2c59b003b00132bb5051a7cfdaa3ac5f52039b
Piotrek <phiwanczuk@gmail.com>
2018-03-22 18:48:25 +0100

    store and state added

---
+++ src/store.js
@@ -0,0 +1,15 @@
+
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import films from './state/films'
+
+const reducers = combineReducers({
+    films
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
+)
