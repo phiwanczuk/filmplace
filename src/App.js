592a87292d56191e1f49888c7d6af8cde2b114a9
Piotrek <phiwanczuk@gmail.com>
2018-03-29 18:30:48 +0200

    temp background added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Films from './components/Films'
+import FilmsSearch from "./components/Films/FilmsSearch";
+import SingleFilm from "./components/SingleFilm/SingleFilm";
 
 
 class App extends Component {
@@ -12,7 +14,9 @@ class App extends Component {
             <Provider store={store}>
                 <Router>
                     <div>
+                        <Route exact path="/" component={FilmsSearch}/>
                         <Route exact path="/" component={Films}/>
+                        <Route path="/film/:id" component={SingleFilm}/>
                     </div>
                 </Router>
             </Provider>
